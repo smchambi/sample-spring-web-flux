dcf21c0eb1a6d426f71433380ae1cd6f89a3aa3b
smchambi <sebastianchambi0.6@gmail.com>
2021-05-22 19:23:23 -0300

    Setup project

---
+++ src/test/java/com/smchambi/sample/SampleSpringWebFluxApplicationTests.java
@@ -0,0 +1,12 @@
+package com.smchambi.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringWebFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
